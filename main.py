f6647ebf031e5432762470a59eee91d6e71e423d
Viktoria Koleva <vicky.koleva03@gmail.com>
2024-02-24 19:00:34 +0200

    base and home template created

--- main.py
+++ main.py
@@ -8,5 +8,11 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = 'fksdhkdshfd'
 socketio = SocketIO(app)
 
+# create route for the home page
+@app.route("/", methods=["GET", "POST"])
+def home():
+    
+    return render_template("home.html")
+
 if __name__ == "__main__":
     socketio.run(app, debug=True)
